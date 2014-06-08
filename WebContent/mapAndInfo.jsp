7c80af78312f9b1fbdb298f0f9144371c9c87e4d
Chiara Brosco <chbrosco@stanford.edu>
2014-06-08 03:23:05 -0700

    changed select to redirect to mapInfo

--- WebContent/mapAndInfo.jsp
+++ WebContent/mapAndInfo.jsp
@@ -1,18 +1,18 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
-    <%@ page import="decameron.*" %>
+    <%@ page import= "decameron.*" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Map Saved</title>
+<%Story st= (Story)session.getAttribute("story"); %>
+
+<title>Day <%=st.getGiorno()%> Story <%=st.getNumber()%> </title>
 <jsp:include page="mapDisplay.jsp"></jsp:include>
 </head>
 <body>
 <jsp:include page="toolbar.jsp"></jsp:include>
-Your Map was saved successfully
-<br>
-<%Story st= (Story)session.getAttribute("story"); %>
+
 Day <%=st.getGiorno() %> Story <%=st.getNumber()%> <br> 
 Storyteller: <%=st.getStoryteller()%><br>
 Ruler: <%=st.getRegina() %><br>
@@ -20,7 +20,6 @@ Extra Info: <%=st.getExtraInfo() %>
 <div id="map-canvas">
 </div>
 <%session.setAttribute("story", null); %>
-
 
 </body>
 </html>
--- src/decameron/SearchServlet.java
+++ src/decameron/SearchServlet.java
@@ -1,26 +1,23 @@
 package decameron;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class SelectServlet
+ * Servlet implementation class SearchServlet
  */
-@WebServlet("/SelectServlet")
-public class SelectServlet extends HttpServlet {
+@WebServlet("/SearchServlet")
+public class SearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public SelectServlet() {
+    public SearchServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -36,19 +33,7 @@ public class SelectServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int giorno =-1;
-		int number= Story.NA;		//if "N/A" selected
-		if(!request.getParameter("giorno").equals("all")) giorno = Integer.parseInt(request.getParameter("giorno"));
-		if(request.getParameter("number").equals("all")){
-			number= -1;
-		}else if(!request.getParameter("number").equals("N/A")){
-			number = Integer.parseInt(request.getParameter("giorno"));
-		}
-		Story st= new Story(giorno, request.getParameter("teller"), number);
-		HttpSession session = request.getSession(true);
-		session.setAttribute("story", st);
-		RequestDispatcher dispatch = request.getRequestDispatcher("mapAndInfo.jsp");
-		dispatch.forward(request, response);
+		// TODO Auto-generated method stub
 	}
 
 }
